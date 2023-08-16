442c2e7a59a955d4693eddb0ea251a09007e6481
tBrause <brause.torsten@gmail.com>
2023-08-16 09:07:23 +0200

    add h1

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 // Render
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+	<React.StrictMode>
+		<h1>Hello</h1>
+	</React.StrictMode>
 );
